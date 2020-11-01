1c5a3d1ff0e2945ea877a6c4c74dcf826790f52a
Dvilson <papiscamara2@gmail.com>
2020-11-01 09:21:41 +0000

    firebase set up

---
+++ app/src/main/java/com/dvilson/healthyadolescent/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.dvilson.healthyadolescent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+    public void mainActivityLaunh(){
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                new Intent(SplashScreen.this,MainActivity.class);
+            }
+        },2000);
+    }
+}
